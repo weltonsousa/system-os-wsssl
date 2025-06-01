2c558ba4179074d5eb2dc390449e5f1759476d54
WeltonSousa <weltonsousa@gmail.com>
2025-06-01 16:34:27 -0300

    get params fix clientes v

--- src/app/api/clientes/[id]/route.ts
+++ src/app/api/clientes/[id]/route.ts
@@ -26,8 +26,8 @@ const clienteUpdateSchema = z.object({
   ativo: z.boolean().optional(),
 });
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-  const id_cliente = params.id;
+export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const id_cliente = (await params).id;
 
   try {
     const cliente = await prisma.cliente.findUnique({
